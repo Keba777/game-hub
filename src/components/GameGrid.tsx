e1b5d76c83a32412ef1a3ae0537d929698ed72ae
Kibreab Aklilu <Akkeba37@gmail.com>
2023-09-29 11:57:56 +0300

    Create a custom hook to fetch the fames

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,35 +1,8 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    axios
-      .get<FetchGamesResponse>("https://api.rawg.io/api/games", {
-        params: {
-          key: "a0ccc7e743fd4bfc8fa444832c73109c",
-        },
-      })
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
